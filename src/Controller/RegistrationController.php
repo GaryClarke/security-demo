133eb7bf4784d3c1e7db693ad523e00f99e0024c
Gary Clarke <gary@pack-net.co.uk>
2021-04-03 14:43:06 +0100

    Add mailer to docker-compose

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -49,14 +49,13 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
 
             // generate a signed url and email it to the user
-//            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
-//                (new TemplatedEmail())
-//                    ->from(new Address('admin@security-demo.com', 'Security'))
-//                    ->to($user->getEmail())
-//                    ->subject('Please Confirm your Email')
-//                    ->htmlTemplate('registration/confirmation_email.html.twig')
-//            );
-            // do anything else you need here, like send an email
+            $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
+                (new TemplatedEmail())
+                    ->from(new Address('admin@security-demo.com', 'Security'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
+            );
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
