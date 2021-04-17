a2f864b106121962d4625550b4ab29b4462fdcfc
Gary Clarke <gary@pack-net.co.uk>
2021-04-17 15:05:35 +0100

    Move admin role check to controller

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,9 +2,11 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('ROLE_ADMIN')]
 class AdminController extends AbstractController
 {
     #[Route('/admin/dashboard', name: 'admin_dashboard')]
